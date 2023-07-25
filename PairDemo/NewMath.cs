2191ea4d84805c69059816b76a7faa283bd0b699
Justin <Justin@grandcircus.co>
2023-07-25 13:56:22 -0400

    first

---
+++ PairDemo/NewMath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairDemo
+{
+    internal class NewMath
+    {
+        public static double Adder(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Subber(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Multipler(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Divider(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
